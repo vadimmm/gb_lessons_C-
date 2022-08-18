473f16ce60b91c93d226591c6cca44fad34b65e3
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-18 18:21:39 +0300

    example005 Ура,Маша

---
+++ example005_LikePeople/Program.cs
@@ -0,0 +1,13 @@
+﻿string likeUserName = "Маша";
+Console.Write("Please, write your username: ");
+string username = Console.ReadLine();
+
+if (username == likeUserName)
+{  
+    Console.WriteLine("Ура, это же " + likeUserName + "!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
