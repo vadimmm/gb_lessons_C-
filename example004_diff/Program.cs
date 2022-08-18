c470ddce44c9126669a67ab3a9395c45db21c8ba
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-18 17:59:27 +0300

    example004 int_to_souble

--- example004_diff/Program.cs
+++ example004_diff/Program.cs
@@ -1,3 +1,7 @@
-﻿int numberA = 10;
-int numberB = 5;
+﻿// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+double numberA = 12;
+double numberB = 5;
 Console.WriteLine(numberA / numberB);
